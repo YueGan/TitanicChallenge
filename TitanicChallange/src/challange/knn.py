30fe70388f17e27eae3f64a162dffd52af87f282
Gavin Gan <Gavin@Big-Machine.hitronhub.home>
2016-12-19 12:05:41 -0500

    Added comments

---
+++ TitanicChallange/src/challange/knn.py
@@ -0,0 +1,47 @@
+"""K-nn Algorithm"""
+
+import pandas as pd
+from scipy.spatial import distance
+
+test = pd.read_csv("Test_Set.csv")
+train = pd.read_csv("Train_Set.csv")
+
+def knn(k,train,test):
+    
+    dist = []
+    ans = 0
+    
+    # Calculate the distance between each variable in test with entire train
+    for i in range (891):
+        dist.append(distance.euclidean(test,train.ix[i,2:]))
+    
+    # Sort the distances
+    index = sorted(range(len(dist)),key=lambda x:dist[x])
+    
+    # Find the top K classes and classify
+    for i in range(0,k):
+        if train.ix[index[i],1]==1:
+            ans += 1
+            
+    if ans > k/2:
+        predict = 1 
+    else:
+        predict = 0
+
+    return predict
+    
+
+result=[]
+# Loop through entire test set with 5-nn
+for i in range(0,418):
+    result.append(knn(5, train, test.ix[i,2:]))
+
+id=[]
+for i in range (0,418):
+    id.append(892+i)
+
+raw_data = {'PassengerId': id,
+            'Survived': result}
+df = pd.DataFrame(raw_data, columns = ['PassengerId','Survived'])
+
+df.to_csv('predict.csv')
---
+++ TitanicChallange/src/challange/train_model.py
@@ -0,0 +1,43 @@
+""" 
+selected train model
+"""
+
+
+import csv as csv
+import numpy as np
+import pandas as pd
+#read train data and test data
+train = pd.read_csv("../../data/train.csv")
+test = pd.read_csv("../../data/train.csv")
+
+# New Column 'Child', 1 if Child, 0 if not 
+train["Child"] = 1
+train.loc[train["Age"]>18,'Child'] = 0
+
+# New Column 'gender', 1 if female, 0 if male
+train["gender"] = 0
+train.loc[train["Sex"]=="female",'gender'] = 1
+train.loc[train["Sex"]=="male",'gender'] = 0
+
+# Select model with only 1 and 0
+del train['Age'],train['PassengerId'],train['SibSp'],train['Parch']
+del train['Ticket'],train['Cabin'],train['Embarked'],train['Name'],train['Sex']
+
+# Output to new data set
+train.to_csv('Train_Set.csv')
+
+# New Column 'Child', 1 if Child, 0 if not 
+test["Child"] = 0
+test.loc[test["Age"]>18,'Child'] =1
+
+# New Column 'gender', 1 if female, 0 if male
+test["gender"] = 0
+test.loc[test["Sex"]=="female",'gender'] = 1
+test.loc[test["Sex"]=="male",'gender'] = 0
+
+del test['Age'],test['PassengerId'],test['SibSp'],test['Parch']
+del test['Ticket'],test['Cabin'],test['Embarked'],test['Name'],test['Sex']
+test = test.fillna(test.mean()) # Fill the empty slots
+
+test.to_csv('Test_Set.csv')
+	
